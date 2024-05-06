3cf047d85e0d8411e969ca930dd57b5a78d5fa51
sryang <sarang628@gmail.com>
2024-05-06 17:36:13 +0800

    update

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -4,51 +4,37 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.myapplication.di.restaurant_detail.ProvideRestaurantScreen
 import com.google.samples.apps.sunflower.ui.TorangTheme
+import com.sarang.instagralleryModule.GalleryNavHost
 import com.sarang.torang.compose.LoginNavHost
-import com.sarang.torang.compose.feed.Feeds
-import com.sarang.torang.compose.feed.MyFeedScreen
-import com.sarang.torang.di.feed_di.review
-import com.sarang.torang.di.main_di.ProvideMainScreen
+import com.sarang.torang.compose.MainScreen
+import com.sarang.torang.compose.ProfileScreenNavHost
+import com.sarang.torang.compose.edit.EditProfileScreen
+import com.sarang.torang.di.bottomsheet.provideFeedMenuBottomSheetDialog
+import com.sarang.torang.di.comment_di.provideCommentBottomDialogSheet
+import com.sarang.torang.di.feed_di.provideFeedScreen
 import com.sarang.torang.di.main_di.ProvideMyFeedScreen
+import com.sarang.torang.di.profile_di.MyProfileScreenNavHost
+import com.sarang.torang.di.report_di.provideReportModal
 import com.sarang.torang.di.torang.ProvideAddReviewScreen
-import com.sarang.torang.di.torang.ProvideEditProfileImageScreen
-import com.sarang.torang.di.torang.ProvideEditProfileScreen
 import com.sarang.torang.di.torang.ProvideModReviewScreen
-import com.sarang.torang.di.torang.ProvideProfileScreen
 import com.sarang.torang.di.torang.ProvideSplashScreen
-import com.sarang.torang.uistate.FeedUiState
-import com.sarang.torang.uistate.FeedsUiState
+import com.sryang.findinglinkmodules.di.finding_di.Finding
 import com.sryang.settings.di.settings.ProvideSettingScreen
+import com.sryang.torang.compose.AlarmScreen
+import com.sryang.torangbottomsheet.di.bottomsheet.provideShareBottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 
@@ -56,8 +42,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Displaying edge-to-edge
-        //WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             TorangTheme {
                 Surface(
@@ -74,22 +58,68 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun TorangScreen() {
         val navController = rememberNavController()
-        val listState = rememberLazyListState()
+        val profileNavController = rememberNavController()
+        var commentDialogShow by remember { mutableStateOf(false) }
         TorangScreen(
             navController = navController,
+            mainScreen = {
+                MainScreen(
+                    feedScreen = provideFeedScreen(
+                        navController = navController,
+                        onAddReview = { navController.navigate("addReview") }
+                    ) { commentDialogShow = true },
+                    findingScreen = { Finding(navController = navController) },
+                    myProfileScreen = {
+                        MyProfileScreenNavHost(
+                            navController = profileNavController,
+                            onSetting = { navController.navigate("settings") },
+                            onEmailLogin = { navController.navigate("emailLogin") },
+                            onReview = {
+                                Log.d("__Main", "MyProfileScreen onReview reviewId : ${it}")
+                                profileNavController.navigate("myFeed/${it}")
+                            },
+                            onClose = { profileNavController.popBackStack() },
+                            myFeed = {
+                                ProvideMyFeedScreen(
+                                    navController = navController,
+                                    reviewId = it.arguments?.getString("reviewId")?.toInt() ?: 0,
+                                    onEdit = { navController.navigate("modReview/${it}") },
+                                    onProfile = { profileNavController.navigate("profile/${it}") },
+                                    onBack = { profileNavController.popBackStack() }
+                                )
+                            }
+                        )
+                    },
+                    alarm = { AlarmScreen(onEmailLogin = {}) },
+                    commentBottomSheet = provideCommentBottomDialogSheet(commentDialogShow) {
+                        commentDialogShow = false
+                    },
+                    menuDialog = provideFeedMenuBottomSheetDialog(),
+                    shareDialog = provideShareBottomSheetDialog(),
+                    reportDialog = provideReportModal(),
+                    onEdit = { navController.navigate("modReview/${it}") }
+                )
+            },
             profileScreen = {
                 Log.d("__TORANG", "move ProvideProfileScreen")
-                ProvideProfileScreen(
-                    navController = navController,
-                    navBackStackEntry = it,
-                    onReview = {
-                        Log.d("__TORANG", "reviewId = ${it}")
-                        navController.navigate("myFeed/${it}")
-                    },
-                    onProfile = {
-                        navController.navigate("profile/${it}")
-                    }
-                )
+                val userId = it.arguments?.getString("id")?.toInt()
+                if (userId != null) {
+                    ProfileScreenNavHost(
+                        id = userId,
+                        onClose = { navController.popBackStack() },
+                        onEmailLogin = { navController.navigate("emailLogin") },
+                        onReview = { profileNavController.navigate("myFeed/${it}") },
+                        myFeed = {
+                            ProvideMyFeedScreen(
+                                navController = profileNavController,
+                                reviewId = it.arguments?.getString("reviewId")?.toInt() ?: 0,
+                                onEdit = { navController.navigate("modReview/${it}") }
+                            )
+                        }
+                    )
+                } else {
+                    Text(text = "사용자 정보가 없습니다.")
+                }
             },
             settings = { ProvideSettingScreen(navController) },
             splashScreen = { ProvideSplashScreen(navController) },
@@ -107,12 +137,23 @@ class MainActivity : ComponentActivity() {
                         }
                     })
             },
-            editProfileScreen = { ProvideEditProfileScreen(navController) },
-            editProfileImageScreen = { ProvideEditProfileImageScreen(navController) },
-            restaurantScreen = { ProvideRestaurantScreen(it) },
-            mainScreen = {
-                ProvideMainScreen(navController = navController)
+            editProfileScreen = {
+                EditProfileScreen(
+                    onEditImage = { navController.navigate("EditProfileImage") },
+                    onBack = {
+                        navController.popBackStack()
+                    }
+                )
             },
+            editProfileImageScreen = {
+                GalleryNavHost(onNext = {
+                    //profileViewModel.updateProfileImage(it[0]) // TODO:: profileViewModel 없이 이미지 업로드 방법 찾기
+                    navController.popBackStack()
+                }, onClose = {
+                    navController.popBackStack()
+                })
+            },
+            restaurantScreen = { ProvideRestaurantScreen(it) },
             modReviewScreen = {
                 ProvideModReviewScreen(
                     navHostController = navController,
@@ -131,59 +172,10 @@ class MainActivity : ComponentActivity() {
             myFeed = {
                 ProvideMyFeedScreen(
                     navController = navController,
-                    reviewId = it.arguments?.getString("reviewId")?.toInt() ?: 0
+                    reviewId = it.arguments?.getString("reviewId")?.toInt() ?: 0,
+                    onEdit = { navController.navigate("modReview/${it}") }
                 )
             }
-        )
-    }
-}
-
-@Preview
-@Composable
-fun shimmerTest() {
-    val showShimmer by remember { mutableStateOf(true) }
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2), content = {
-            items(2) {
-                Box(
-                    modifier = Modifier
-                        .size(200.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(
-                            shimmerBrush(showShimmer, 1300f)
-                        )
-                )
-            }
-        })
-}
-
-@Composable
-fun shimmerBrush(showShimmer: Boolean = true, targetValue: Float = 1000f): Brush {
-    return if (showShimmer) {
-        val shimmerColors = listOf(
-            Color.LightGray.copy(alpha = 0.6f),
-            Color.LightGray.copy(alpha = 0.2f),
-            Color.LightGray.copy(alpha = 0.6f),
-        )
-
-        val transition = rememberInfiniteTransition(label = "")
-        val translateAnimation = transition.animateFloat(
-            initialValue = 0f,
-            targetValue = targetValue,
-            animationSpec = infiniteRepeatable(
-                animation = tween(800), repeatMode = RepeatMode.Reverse
-            ), label = ""
-        )
-        Brush.linearGradient(
-            colors = shimmerColors,
-            start = Offset.Zero,
-            end = Offset(x = translateAnimation.value, y = translateAnimation.value)
-        )
-    } else {
-        Brush.linearGradient(
-            colors = listOf(Color.Transparent, Color.Transparent),
-            start = Offset.Zero,
-            end = Offset.Zero
         )
     }
 }
