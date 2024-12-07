5d8f17e40dfdc326cf61e944c31fba7d0a620128
bingley1 <android1@naver.com>
2024-12-07 15:23:13 +0900

    update submodule : torangBottomSheet, instaGramGallery

--- app/src/main/java/com/sarang/torang/di/bottomsheet/MenuModule.kt
+++ app/src/main/java/com/sarang/torang/di/bottomsheet/MenuModule.kt
@@ -1,11 +1,9 @@
-package com.sarang.torang.di.bottomsheet
+package com.sarang.torangbottomsheet.di.bottomsheet
 
 import android.util.Log
-import androidx.compose.runtime.Composable
 import com.sarang.torang.data.dao.LoggedInUserDao
 import com.sarang.torang.data.dao.ReviewDao
-import com.sryang.torang.compose.bottomsheet.feed.FeedMenuBottomSheetDialog
-import com.sryang.torang.usecase.IsMyReviewUseCase
+import com.sarang.torang.usecase.IsMyReviewUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -35,15 +33,3 @@ class MenuModule {
         }
     }
 }
-
-fun provideFeedMenuBottomSheetDialog(): @Composable (reviewId: Int, onClose: () -> Unit, onReport: (Int) -> Unit, onDelete: (Int) -> Unit, onEdit: (Int) -> Unit) -> Unit =
-    { reviewId, onClose, onReport, onDelete, onEdit ->
-        FeedMenuBottomSheetDialog(
-            isExpand = true,
-            reviewId = reviewId,
-            onReport = { onReport.invoke(reviewId) },
-            onDelete = { onDelete.invoke(reviewId) },
-            onEdit = { onEdit.invoke(reviewId) },
-            onClose = onClose
-        )
-    }
--- app/src/main/java/com/sarang/torang/di/bottomsheet/ShareModule.kt
+++ app/src/main/java/com/sarang/torang/di/bottomsheet/ShareModule.kt
@@ -1,13 +1,10 @@
-package com.sryang.torangbottomsheet.di.bottomsheet
+package com.sarang.torangbottomsheet.di.bottomsheet
 
-import androidx.compose.runtime.Composable
-import com.sarang.torang.BuildConfig
 import com.sarang.torang.api.ApiProfile
 import com.sarang.torang.data.dao.LoggedInUserDao
 import com.sarang.torang.session.SessionService
-import com.sryang.torang.compose.bottomsheet.share.ShareBottomSheetDialog
-import com.sryang.torang.data.User
-import com.sryang.torang.usecase.GetFollowerUseCase
+import com.sarang.torang.data.User
+import com.sarang.torang.usecase.GetFollowerUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,30 +13,24 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-class ShareModule {
+class ShareModule
+{
     @Provides
-    fun providesGetFollowerUseCase(
-        apiProfile: ApiProfile,
-        loggedInUserDao: LoggedInUserDao,
-        sessionService: SessionService
-    ): GetFollowerUseCase {
-        return object : GetFollowerUseCase {
-            override suspend fun invoke(): List<User> {
+    fun providesGetFollowerUseCase(apiProfile: ApiProfile, loggedInUserDao: LoggedInUserDao, sessionService: SessionService): GetFollowerUseCase
+    {
+        return object : GetFollowerUseCase
+        {
+            override suspend fun invoke(): List<com.sarang.torang.data.bottomsheet.User>
+            {
                 val token = sessionService.getToken() ?: throw Exception("로그인을 해주세요.")
 
-                return apiProfile.getMyFollowing(token).map {
+                /*return apiProfile.getMyFollowing(token).map {
                     User(userId = it.followerId, userName = it.userName, picture = it.profilePicUrl)
-                }
+                }*/
+
+                return listOf()
             }
         }
     }
-}
 
-fun provideShareBottomSheetDialog(): @Composable (onClose: () -> Unit) -> Unit = {
-    ShareBottomSheetDialog(
-        profileServerUrl = BuildConfig.PROFILE_IMAGE_SERVER_URL,
-        isExpand = true,
-        onSelect = {},
-        onClose = it
-    )
 }
