cf6d272b597e03d7f34336af2a277f0f8173007e
sryang <sarang628@gmail.com>
2025-04-02 12:15:30 +0900

    add bottom sheet new feature
    - add dim in bottom sheet

--- app/src/main/java/com/sarang/torang/di/filter/GetCitiesByNationIdUseCaseImpl.kt
+++ app/src/main/java/com/sarang/torang/di/filter/GetCitiesByNationIdUseCaseImpl.kt
@@ -1,10 +1,8 @@
 package com.sryang.findinglinkmodules.di.filter
 
 import com.sarang.torang.api.ApiFilter
-import com.sarang.torang.data.remote.response.CityApiModel
 import com.sryang.screen_filter.data.City
 import com.sryang.screen_filter.usecase.GetCitiesByNationIdUseCase
-import com.sryang.screen_filter.usecase.GetCitiesUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,8 +14,8 @@ class GetCitiesByNationIdUseCaseImpl {
     @Provides
     fun providesGetCitiesUseCase(apiFilter: ApiFilter): GetCitiesByNationIdUseCase {
         return object : GetCitiesByNationIdUseCase {
-            override suspend fun invoke(id: Int): List<City> {
-                return apiFilter.getCitiesByNationId(id).map { it.toCity() }
+            override suspend fun invoke(nationId: Int): List<City> {
+                return apiFilter.getCitiesByNationId(nationId).map { it.toCity() }
             }
         }
     }
