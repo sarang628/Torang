e2733df8bebe1d4d1e99a777652380efda9d1ae0
sryang <sarang628@gmail.com>
2025-08-22 15:44:17 +0800

    adjust recent basefeed

--- app/src/main/java/com/sarang/torang/di/pulltorefresh/providePullToRefreshLayout.kt
+++ app/src/main/java/com/sarang/torang/di/pulltorefresh/providePullToRefreshLayout.kt
@@ -1,6 +1,7 @@
 package com.sarang.torang.di.pulltorefresh
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.sryang.library.pullrefresh.PullToRefreshLayout
 import com.sryang.library.pullrefresh.PullToRefreshLayoutState
 import com.sryang.library.pullrefresh.RefreshIndicatorState
@@ -23,3 +24,20 @@ fun providePullToRefreshLayout(state : PullToRefreshLayoutState): @Composable (B
         }
     }
 }
+
+
+fun providePullToRefresh(state: PullToRefreshLayoutState): @Composable ((modifier : Modifier, isRefreshing: Boolean, onRefresh: (() -> Unit), contents: @Composable (() -> Unit)) -> Unit) =
+    { modifier, isRefreshing, onRefresh, contents ->
+
+        if (isRefreshing) {
+            state.updateState(RefreshIndicatorState.Refreshing)
+        } else {
+            state.updateState(RefreshIndicatorState.Default)
+        }
+
+        PullToRefreshLayout(
+            modifier = Modifier, pullRefreshLayoutState = state, refreshThreshold = 80, onRefresh = onRefresh
+        ) {
+            contents.invoke()
+        }
+    }
