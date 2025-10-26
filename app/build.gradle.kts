423234ce5763f704e9c107f4877950f56ad68685
sryang <sarang628@gmail.com>
2025-10-26 10:38:45 +0900

    update feed grid module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunner = "com.sarang.torang.CustomTestRunner"
 
-        versionCode = 102
+        versionCode = 103
         versionName = "0.1.0"
     }
 
