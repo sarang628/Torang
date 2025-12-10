7bcbf899c2992ad64b76b02b103c8bc27c37afab
sryang <sarang628@gmail.com>
2025-12-10 12:43:15 +0900

    add scaffold in setting screen

--- app/src/main/java/com/sarang/torang/di/settings/provideSettingScreen.kt
+++ app/src/main/java/com/sarang/torang/di/settings/provideSettingScreen.kt
@@ -1,12 +1,20 @@
 package com.sarang.torang.di.settings
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.torangscreensettings.compose.SettingsScreen
 import com.sarang.torang.RootNavController
 
 fun provideSettingScreen(rootNavController: RootNavController): @Composable () -> Unit = {
-    SettingsScreen(
-        onLogout = rootNavController::splash,
-        onBack = rootNavController::popBackStack
-    )
+    Scaffold {
+        Box(Modifier.padding(it)){
+            SettingsScreen(
+                onLogout = rootNavController::splash,
+                onBack = rootNavController::popBackStack
+            )
+        }
+    }
 }
