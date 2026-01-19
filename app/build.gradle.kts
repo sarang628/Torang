88e7d4b261961d226f975a99371738dcbd0ea0ad
sryang <sarang628@gmail.com>
2026-01-19 09:58:45 +0900

    update modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunner = "com.sarang.torang.CustomTestRunner"
 
-        versionCode = 137
+        versionCode = 138
         versionName = "0.1.0"
     }
 
