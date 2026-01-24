94d56f959264fbfbdf4bdf067c1442f9303fd740
sryang <sarang628@gmail.com>
2026-01-24 20:54:01 +0900

    add order

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunner = "com.sarang.torang.CustomTestRunner"
 
-        versionCode = 139
+        versionCode = 141
         versionName = "0.1.0"
     }
 
