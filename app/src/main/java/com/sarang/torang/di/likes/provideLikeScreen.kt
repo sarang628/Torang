6a5e9f25d97b8ab5266763e69e0cc1e28a0fe06e
sryang <sarang628@gmail.com>
2026-02-09 23:36:13 +0900

    add video review

--- app/src/main/java/com/sarang/torang/di/likes/provideLikeScreen.kt
+++ app/src/main/java/com/sarang/torang/di/likes/provideLikeScreen.kt
@@ -1,14 +1,23 @@
 package com.sarang.torang.di.likes
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.sarang.library.LikeScreen
 import com.sarang.torang.RootNavController
+import com.sarang.torang.di.image.TorangAsyncImageData
 import com.sarang.torang.di.image.provideTorangAsyncImage
 
 fun provideLikeScreen(rootNavController: RootNavController): @Composable (Int) -> Unit =
     { reviewId ->
         LikeScreen(
-            image = provideTorangAsyncImage(),
+            image = { modifier, model, progressSize, errorIconSize, contentScale -> provideTorangAsyncImage().invoke(TorangAsyncImageData(
+                modifier = modifier,
+                model = model,
+                progressSize = progressSize ?: 30.dp,
+                errorIconSize = errorIconSize?: 30.dp,
+                contentScale = contentScale ?: ContentScale.None
+            )) },
             reviewId = reviewId,
             onImage = rootNavController::profile,
             onName = rootNavController::profile,
