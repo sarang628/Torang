0a2baf919b17417714e95be0a8db53cf0136e8d5
sryang <sarang628@gmail.com>
2026-03-16 12:27:26 +0900

    update modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunner = "com.sarang.torang.CustomTestRunner"
 
-        versionCode = 150
+        versionCode = 151
         versionName = "0.1.0"
 
         buildConfigField("String", "SERVER_URL", "\"http://sarang628.iptime.org\"")
