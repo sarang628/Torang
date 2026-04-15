5e29be1924a37e311e3c6d202d745c6cd6f7ab25
sryang <sarang628@gmail.com>
2026-04-15 14:02:08 +0900

    update modules

--- app/src/main/java/com/sarang/torang/di/settings/provideSettingScreen.kt
+++ app/src/main/java/com/sarang/torang/di/settings/provideSettingScreen.kt
@@ -1,20 +1,12 @@
 package com.sarang.torang.di.settings
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.sarang.torang.RootNavController
 import com.sarang.torang.compose.SettingsScreen
 
 fun provideSettingScreen(rootNavController: RootNavController): @Composable () -> Unit = {
-    Scaffold {
-        Box(Modifier.padding(it)){
-            SettingsScreen(
-                onLogout = rootNavController::splash,
-                onBack = rootNavController::popBackStack
-            )
-        }
-    }
+    SettingsScreen(
+        onLogout = rootNavController::splash,
+        onBack = rootNavController::popBackStack
+    )
 }
